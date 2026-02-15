08597346ee9473ef01949ae1fa1836e671e6a70e
freelinex <freelinex7@gmail.com>
2026-02-15 20:07:00 +0200

    make build

---
+++ index.f0f24a61.js
@@ -0,0 +1,2 @@
+document.addEventListener("DOMContentLoaded",function(){var e=document.querySelector(".wall"),t=document.querySelector(".spider");null!==e&&null!==t&&(t.style.left=(e.clientWidth-t.clientWidth)/2+"px",t.style.top=(e.clientHeight-t.clientHeight)/2+"px")});
+//# sourceMappingURL=index.f0f24a61.js.map
