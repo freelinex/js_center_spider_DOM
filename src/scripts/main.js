34a86183e1b9976f2870c02457bf999a30514b50
freelinex <freelinex7@gmail.com>
2026-02-15 20:06:45 +0200

    correcting remarks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const wall = document.querySelector('.wall');
-const spider = document.querySelector('.spider');
+document.addEventListener('DOMContentLoaded', () => {
+  const wall = document.querySelector('.wall');
+  const spider = document.querySelector('.spider');
 
-if (wall !== null) {
-  spider.style.left = (wall.clientWidth - spider.clientWidth) / 2 + 'px';
-  spider.style.top = (wall.clientHeight - spider.clientHeight) / 2 + 'px';
-}
+  if (wall !== null && spider !== null) {
+    spider.style.left = (wall.clientWidth - spider.clientWidth) / 2 + 'px';
+    spider.style.top = (wall.clientHeight - spider.clientHeight) / 2 + 'px';
+  }
+});
