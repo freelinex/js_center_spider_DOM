905792af0721b76b0df215c1feccbd26d41e7fa4
freelinex <freelinex7@gmail.com>
2026-02-15 19:49:44 +0200

    added null check

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,5 +3,7 @@
 const wall = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-spider.style.left = (wall.clientWidth - spider.clientWidth) / 2 + 'px';
-spider.style.top = (wall.clientHeight - spider.clientHeight) / 2 + 'px';
+if (wall !== null) {
+  spider.style.left = (wall.clientWidth - spider.clientWidth) / 2 + 'px';
+  spider.style.top = (wall.clientHeight - spider.clientHeight) / 2 + 'px';
+}
